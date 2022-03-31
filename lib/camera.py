33f89dfc8be9c7e579751901963f3f9a6cd8f4a0
sktkddn777 <sktkddn777@naver.com>
2022-03-31 19:13:33 +0900

    add http request to dest_url

--- lib/camera.py
+++ lib/camera.py
@@ -5,7 +5,9 @@ import cv2
 from utils.datasets import letterbox
 from utils.general import non_max_suppression, scale_coords
 from utils.plots import Annotator
+import requests
 
+DEST_URL = "destination url for sending fire occur or not"
 
 MODEL_PATH = 'runs/train/exp4/weights/e50b32.pt'
 img_size = 416
@@ -33,20 +35,28 @@ class Camera(object):
     self.me = tello.Tello()
     self.me.connect()
     self.me.streamon()
+  
+  def send_format(self, type):
+    '''
+    0 : fire
+    1 : smoke
+    '''
+    return {'data_type:', type}
     
   def start_cam(self):
     cap = self.me.get_frame_read()
     while True:
-    #ret, img = cap.read()
+
+      if cv2.waitKey(1) == ord('q'):
+        break
+
       img = cap.frame
-      # if not ret:
-      #     break
 
       # preprocess
-      img_input = letterbox(img, img_size, stride=stride)[0]
-      img_input = img_input.transpose((2, 0, 1))[::-1]
-      img_input = np.ascontiguousarray(img_input)
-      img_input = torch.from_numpy(img_input).to(device)
+      img_input = letterbox(img, img_size, stride=stride)[0] # padding
+      img_input = img_input.transpose((2, 0, 1))[::-1] # BGR to RGB
+      img_input = np.ascontiguousarray(img_input) 
+      img_input = torch.from_numpy(img_input).to(device) # torch Tensor형식 변환
       img_input = img_input.float()
       img_input /= 255.
       img_input = img_input.unsqueeze(0)
@@ -59,7 +69,7 @@ class Camera(object):
 
       pred = pred.cpu().numpy()
 
-      pred[:, :4] = scale_coords(img_input.shape[2:], pred[:, :4], img.shape).round()
+      pred[:, :4] = scale_coords(img_input.shape[2:], pred[:, :4], img.shape).round() # img size에 맞게 rescailing
 
       annotator = Annotator(img.copy(), line_width=3, example=str(class_names), font='data/malgun.ttf')
 
@@ -67,14 +77,15 @@ class Camera(object):
           class_name = class_names[int(p[5])]
 
           x1, y1, x2, y2 = p[:4]
-          print(p)
+          
+          # detect fire or smoke
+          if p[5]:
+            r = requests.post(url=DEST_URL, data = self.send_format(p[5]))
+          
           annotator.box_label([x1, y1, x2, y2], '%s %d' % (class_name, float(p[4]) * 100), color=colors[int(p[5])])
 
       result_img = annotator.result()
 
       cv2.imshow('result', result_img)
-      # out.write(result_img)
-      if cv2.waitKey(1) == ord('q'):
-          break
 
     cap.release()
